ae86934478202757bdac6d6d4667b5157e5d1e08
NPException <npexception42@gmail.com>
2023-10-07 16:25:27 +0200

    copy build to simulator data folder

--- build-pdx.bat
+++ build-pdx.bat
@@ -4,4 +4,9 @@ for %%I in (.) do set CurrDirName=%%~nxI
 rmdir /S /Q builds
 mkdir builds
 %PLAYDATE_SDK_PATH%\bin\pdc src\pdx ".\builds\%CurrDirName%"
-exit /b %ERRORLEVEL%
+if NOT %ERRORLEVEL% == 0 exit /b %ERRORLEVEL%
+: copy the built game to the Simulator (so it can be seen in the main menu)
+echo == Copying Build to Simulator ==
+rmdir /S /Q "%PLAYDATE_SDK_PATH%\Disk\Games\User\%CurrDirName%.pdx"
+robocopy /MIR ".\builds\%CurrDirName%.pdx" "%PLAYDATE_SDK_PATH%\Disk\Games\User\%CurrDirName%.pdx" > nul
+exit /b 0
